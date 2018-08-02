fecc81d557e046c875a0326b16f1edac085d5611
laureliosan <41914123+laureliosan@users.noreply.github.com>
2018-08-02 16:38:37 -0300

    Add files via upload

---
+++ Planilha.py
@@ -0,0 +1,56 @@
+import xlrd
+
+workbook = xlrd.open_workbook("teste.xlsx")
+
+worksheet = workbook.sheet_by_name("Planilha1")
+
+#worksheet = workbook.sheet_by_index(0)
+
+
+#(worksheet.col(0)):
+
+pns = (worksheet.col_values(0))
+
+print("Bem vindo ao Alfa. Digite ola() para começar ")
+
+
+def ola():
+    x = input("Agora, qual PN deseja consultar? ")
+    x = str(x)
+    print("")
+    if x in pns:
+        print("O PN esta na Price-list Brasil")
+        print("")
+        return ola()
+    else:
+        print("O PN nao esta na Price-list Brasil ou ele foi digitado errado ")
+        print("")
+        return ola()
+
+
+
+
+
+
+
+
+
+          
+
+
+
+
+
+          
+        
+
+		
+
+		
+
+
+
+
+
+
+
